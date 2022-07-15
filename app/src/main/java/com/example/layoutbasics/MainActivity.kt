5692f73c9de61760663291faef0a8e7a3dd18a04
Robert Gitei <robert.gitei@yopeso.com>
2022-07-15 16:21:14 +0300

    Adds button to log input text

--- app/src/main/java/com/example/layoutbasics/MainActivity.kt
+++ app/src/main/java/com/example/layoutbasics/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.layoutbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.layoutbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnApply.setOnClickListener {
+            val firstName = binding.etFirstName.text
+            val lastName = binding.etLastName.text
+            val birthDate = binding.etBirthDate.text
+            val country = binding.etCountry.text
+            Log.d("MainActivity", "$firstName $lastName born on $birthDate, from $country")
+        }
     }
 }
